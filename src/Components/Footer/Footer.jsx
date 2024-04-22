8a5f7e6cac4e7c80d158dbc342e99020f2a0e367
Rohit Tarang R <rohittarang512005@gmail.com>
2024-04-22 18:07:04 +0530

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,29 +5,44 @@ import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-black py-8">
-      <div className="container mx-auto px-4">
-        <div className="flex max-lg:flex-col flex-row items-center md:justify-between max-lg:space-y-5">
-          <Link to="/" className="flex items-center space-x-2 mb-4 md:mb-0">
-            <img src={logo} alt="Logo" className="w-7" />
-            <span className="font-bold text-white">Meme Viewer</span>
-          </Link>
+    <>
+      <footer className="bg-black py-8">
+        <div className="container mx-auto px-4">
+          <div className="flex max-lg:flex-col flex-row items-center md:justify-between max-lg:space-y-5">
+            <Link to="/" className="flex items-center space-x-2 mb-4 md:mb-0">
+              <img src={logo} alt="Logo" className="w-7" />
+              <span className="font-bold text-white">Meme Viewer</span>
+            </Link>
 
-          <nav className="flex flex-wrap justify-center md:justify-end space-x-5 mb-4 md:mb-0">
-            <FooterLink to="#" text="Privacy Policy" />
-            <FooterLink to="#" text="Terms of Service" />
-            <FooterLink to="#" text="Contact Us" />
-            <FooterLink to="#" text="About Us" />
-          </nav>
+            <nav className="flex flex-wrap justify-center md:justify-end space-x-5 mb-4 md:mb-0">
+              <FooterLink to="#" text="Privacy Policy"  />
+              <FooterLink to="#" text="Terms of Service"  />
+              <FooterLink to="/contact" text="Contact Us"  />
+              <FooterLink to="/about" text="About Us"  />
+            </nav>
 
-          <div className="flex justify-center max-lg:py-2 space-x-5">
-            <SocialIcon icon={FaFacebook} />
-            <SocialIcon icon={FaTwitter} />
-            <SocialIcon icon={FaInstagram} />
+            <div className="flex justify-center max-lg:py-2 space-x-5">
+              <SocialIcon icon={FaFacebook} />
+              <SocialIcon icon={FaTwitter} />
+              <SocialIcon icon={FaInstagram} />
+            </div>
           </div>
         </div>
+      </footer>
+      <div className="text-center mx-auto flex justify-center items-center w-full space-x-2 bg-black text-white p-3 max-[860px]:flex-col ">
+        <p>
+          This meme generator made by Tarang Rohit ! Contact me if you want any
+          types of websites{" "}
+        </p>
+        <a
+          href="https://www.instagram.com/rohitt.tarang/"
+          className="text-[#e13053] hover:bg-white p-2 rounded-full transition-all duration-300"
+        >
+          {/* <span className="">Instagram</span> */}
+          <FaInstagram />
+        </a>
       </div>
-    </footer>
+    </>
   );
 };
 
@@ -35,7 +50,7 @@ const FooterLink = ({ to, text }) => {
   return (
     <Link
       to={to}
-      className="font-medium text-white hover:text-gray-400 transition-colors"
+      className="font-medium text-white hover:bg-white rounded-full hover:text-black transition-colors duration-300 py-1 px-2"
     >
       {text}
     </Link>
